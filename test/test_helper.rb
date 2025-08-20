29374c5c7c99158840c4f2b0d714926d42c69e02
danudeth2004 <oat25482547@gmail.com>
2025-08-20 09:15:50 +0700

    build: Install simplecov, rspec, gitignore coverage and config rails_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,15 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/bin/"
+  add_filter "/db/"
+  add_filter "/config/"
+  add_filter "/vendor/"
+  add_filter "app/mailers/"
+  add_filter "app/jobs/"
+end
 
 module ActiveSupport
   class TestCase
