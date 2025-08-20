1f815f77c5ae90b9afcfc2029be868b6da571480
danudeth2004 <oat25482547@gmail.com>
2025-08-20 09:29:43 +0700

    feat: Validate title not null in model

--- test/models/quest_test.rb
+++ test/models/quest_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class QuestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be quest save when title valid" do
+    quest = Quest.new(title: "Academy Ruby Project", is_complete: false)
+    assert quest.save
+  end
+
+  test "should be quest not save when title invalid" do
+    quest = Quest.new(is_complete: false)
+    assert_not quest.save
+  end
 end
